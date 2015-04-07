e7efe33c9da774d3e6b3784c2d890ce850e946d2
ilyabo <Ilya Boyandin>
2015-04-07 16:09:46 -0400

    moved webpack config into make.js

--- make.js
+++ make.js
@@ -1,12 +1,14 @@
 'use strict';
 
+var webpack = require('webpack');
+var bourbon = require('node-bourbon').includePaths;
+
 var mountFolder = function (connect, dir) {
   return connect.static(require('path').resolve(dir));
 };
 
 
-var webpackDistConfig = require('./webpack.dist.config.js'),
-    webpackDevConfig = require('./webpack.config.js');
+var webpackDistConfig = require('./webpack.dist.config.js');
 
 module.exports = function (grunt) {
   // Let *load-grunt-tasks* require everything
@@ -30,7 +32,51 @@ module.exports = function (grunt) {
       options: {
         hot: true,
         port: 8008,
-        webpack: webpackDevConfig,
+        webpack: {
+          output: {
+            filename: 'main.js',
+            publicPath: '/assets/'
+          },
+
+          cache: true,
+          debug: true,
+          devtool: false,
+          entry: [
+              'webpack/hot/only-dev-server',
+              './src/js/main.js'
+          ],
+
+          stats: {
+            colors: true,
+            reasons: true
+          },
+
+          resolve: {
+            extensions: ['', '.js', '.jsx'],
+            modulesDirectories: [ 'node_modules', 'src/js' ]
+          },
+          module: {
+            loaders: [{
+              test: /\.js$/,
+              exclude: /node_modules|snippets/,
+              loader: 'react-hot!jsx-loader?harmony!babel'
+            },
+            { test: /\.jsx$/, loader: 'jsx-loader?harmony' },
+            { test: /\.scss/, loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded&includePaths[]='+ bourbon},
+            {
+              test: /\.css$/,
+              loader: 'style-loader!css-loader'
+            }, {
+              test: /\.(png|jpg)$/,
+              loader: 'url-loader?limit=8192'
+            }]
+          },
+
+          plugins: [
+            new webpack.HotModuleReplacementPlugin(),
+            new webpack.NoErrorsPlugin()
+          ]
+        },
         publicPath: '/assets/',
         contentBase: './<%= pkg.src %>/'
       },
